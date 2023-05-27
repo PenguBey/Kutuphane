1ca235c26e202436a31fb20addf03622d06d245b
PenguBey <omer39ctnr@gmail.com>
2023-05-27 18:29:58 +0300

    büyük update

--- Kutuphane Proje/WindowsFormsApp1/FormKitaplar.Designer.cs
+++ Kutuphane Proje/WindowsFormsApp1/FormKitaplar.Designer.cs
@@ -29,17 +29,67 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button_geriver = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonsil = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(287, 119);
+            this.listView1.Location = new System.Drawing.Point(264, 136);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(273, 201);
+            this.listView1.Size = new System.Drawing.Size(524, 302);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad";
+            this.columnHeader1.Width = 138;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Yazar";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 159;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tür";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Durum";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 103;
             // 
             // label1
             // 
@@ -51,11 +101,202 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "hepsi",
+            "roman",
+            "biyografi",
+            "anı",
+            "hikaye",
+            "fabl",
+            "masal"});
+            this.comboBox2.Location = new System.Drawing.Point(70, 136);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(88, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = " Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " Yazar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tür";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(735, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = " Durum";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(264, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(411, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = " ARA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(703, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = " Elimdekiler";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(703, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = " Alabilirim";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(703, 81);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Text = " Alamam";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(703, 104);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = " Hepsi";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button_geriver
+            // 
+            this.button_geriver.Location = new System.Drawing.Point(264, 43);
+            this.button_geriver.Name = "button_geriver";
+            this.button_geriver.Size = new System.Drawing.Size(75, 23);
+            this.button_geriver.TabIndex = 9;
+            this.button_geriver.Text = " Geri Ver";
+            this.button_geriver.UseVisualStyleBackColor = true;
+            this.button_geriver.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(345, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ödünç Al";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(237, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "....";
+            // 
+            // buttonsil
+            // 
+            this.buttonsil.Enabled = false;
+            this.buttonsil.Location = new System.Drawing.Point(28, 225);
+            this.buttonsil.Name = "buttonsil";
+            this.buttonsil.Size = new System.Drawing.Size(75, 23);
+            this.buttonsil.TabIndex = 11;
+            this.buttonsil.Text = " Sil";
+            this.buttonsil.UseVisualStyleBackColor = true;
+            this.buttonsil.Visible = false;
+            this.buttonsil.Click += new System.EventHandler(this.buttonsil_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(126, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = " Ekle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // FormKitaplar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonsil);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button_geriver);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Name = "FormKitaplar";
@@ -70,5 +311,26 @@
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button_geriver;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonsil;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Kutuphane Proje/WindowsFormsApp1/FormKitaplar.cs
+++ Kutuphane Proje/WindowsFormsApp1/FormKitaplar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,211 @@ namespace WindowsFormsApp1
         private void FormKitaplar_Load(object sender, EventArgs e)
         {
             label1.Text = "Kullanıcı: " + sql.adim;
+            if (sql.adim == "admin")
+            {
+                button_geriver.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = true;
+                buttonsil.Enabled = true;
+                button_geriver.Visible = false;
+                button3.Visible = false;
+                button4.Visible = true;
+                buttonsil.Visible = true;
+            }
+            comboBox2.SelectedIndex = 0;
+            sql.Baglantı();
+            sql.komut.CommandText = "select * from Table_kitaplar";
+            sorgulamak();
+
+            sql.baglan.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sql.Baglantı();
+            listView1.Items.Clear();
+            sql.komut.Parameters.Clear();
+            string sorgu = "select * from Table_kitaplar where ";
+            List<string> filtreler = new List<string>();
+
+            if (textBox1.Text == "" && textBox2.Text =="" && comboBox2.SelectedIndex == 0 && radioButton4.Checked == true)
+            {
+                sorgu = "select * from Table_kitaplar";
+            }
+
+            if (textBox1.Text != "")
+            {
+                filtreler.Add(" ad like @adim ");
+                sql.komut.Parameters.AddWithValue("adim", "%" + textBox1.Text + "%");
+            }
+            if (textBox2.Text != "")
+            {
+                filtreler.Add(" yazar like @yazarım ");
+                sql.komut.Parameters.AddWithValue("yazarım", "%" + textBox2.Text + "%");
+            }
+            if (comboBox2.SelectedIndex != 0 && comboBox2.SelectedItem.ToString() != "")
+            {
+                filtreler.Add(" tür = @türüm ");
+                sql.komut.Parameters.AddWithValue("türüm", comboBox2.SelectedItem.ToString());
+            }
+
+
+            if (radioButton1.Checked == true)
+            {
+                filtreler.Add(" durum = @durumum ");
+                sql.komut.Parameters.AddWithValue("durumum", sql.adim);
+            }
+            if (radioButton2.Checked == true)
+            {
+                filtreler.Add(" durum  is null ");
+
+            }
+            if (radioButton3.Checked == true)
+            {
+                filtreler.Add(" durum is not null and durum != @durumum ");
+                sql.komut.Parameters.AddWithValue("durumum", sql.adim);
+            }
+            sorgu += string.Join("and", filtreler);
+            sql.komut.CommandText = sorgu;
+            sorgulamak();
+            sql.baglan.Close();
+            
+        }
+        private void sorgulamak()
+        {
+            SqlDataReader oku = sql.komut.ExecuteReader();
+            while (oku.Read())
+            {
+                if (oku["durum"].ToString() == "")
+                {
+                    string[] yukle = { oku.GetString(0), oku.GetString(1), oku["tür"].ToString(), "Ödünç Alınabilir" };
+                    ListViewItem eke = new ListViewItem(yukle);
+                    listView1.Items.Add(eke);
+                }
+                else if (oku["durum"].ToString() == sql.adim)
+                {
+                    
+                    string[] yukle = { oku.GetString(0), oku.GetString(1), oku["tür"].ToString(), "Senin Elinde" };
+
+                    ListViewItem eke = new ListViewItem(yukle);
+                    listView1.Items.Add(eke);
+                }
+                else
+                {
+                    string[] yukle;
+                    if (sql.adim == "admin")
+                    {
+                         yukle = new string[] { oku.GetString(0), oku.GetString(1), oku["tür"].ToString(), oku["durum"].ToString() };
+                    }
+                    else
+                    {
+                        yukle = new string[] { oku.GetString(0), oku.GetString(1), oku["tür"].ToString(), "alınamaz" };
+
+                    }
+                    
+                    ListViewItem eke = new ListViewItem(yukle);
+                    listView1.Items.Add(eke);
+                }
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                sql.Baglantı();
+                sql.komut.Parameters.Clear();
+                sql.komut.CommandText = "select durum from Table_kitaplar where ad = @adim and durum = @ durumum";
+                sql.komut.Parameters.AddWithValue("adim", textBox1.Text);
+                
+                SqlDataReader oku = sql.komut.ExecuteReader();
+                if (oku.Read())
+                {
+                   if (oku["durum"].ToString() =="" )
+                   {
+                  
+                       sql.baglan.Close();
+                       sql.Baglantı();
+                       sql.komut.CommandText = "update Table_kitaplar set durum = @durumum where ad = @adim";
+                       sql.komut.Parameters.AddWithValue("durumum", sql.adim);
+                       sql.komut.Parameters.AddWithValue("adim", textBox1.Text);
+                       sql.komut.ExecuteNonQuery();
+                       label6.Text = "kitabınız başarıyla ödünç alındı";
+                  
+                   }
+                   else
+                   {
+                       label6.Text = "bu kitap başkasında yada şuan sende";
+                   }
+                  
+                  
+                  
+                    
+                }
+                else
+                {
+                    label6.Text = "Kitabın adını yanlış yazdınız";
+                }
+
+
+                sql.baglan.Close();
+            }
+            else
+            {
+                label6.Text = "Ödünç alacağınız kitabın adını, Ad kısmına eksiksin yazınız ";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                sql.Baglantı();
+                sql.komut.Parameters.Clear();
+                sql.komut.CommandText = "select * from Table_kitaplar where ad = @adim ";
+                sql.komut.Parameters.AddWithValue("adim", textBox1.Text);
+                SqlDataReader oku = sql.komut.ExecuteReader();
+                if (oku.Read())
+                {
+
+                    if (oku["durum"].ToString() == sql.adim)
+                    {
+                        sql.baglan.Close();
+                        sql.Baglantı();
+                        sql.komut.CommandText = "update Table_kitaplar set durum = null where durum = @durumum";
+                        sql.komut.Parameters.AddWithValue("durumum", sql.adim);
+                        sql.komut.ExecuteNonQuery();
+                        label6.Text = "kitabınız başarıyla iade edildi";
+                    }
+                    else
+                    {
+                        label6.Text = "sende olmayan kitabı geri veremessin";
+                    }
+
+                }
+                else
+                {
+                    label6.Text = "Kitabın adını yanlış yazdınız";
+                }
+
+                sql.baglan.Close();
+            }
+            else
+            {
+                label6.Text = "Ödünç vereceğiniz kitabın adını, Ad kısmına eksiksin yazınız ";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonsil_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
